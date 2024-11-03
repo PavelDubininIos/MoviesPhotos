e5cbd72f0137523adf9237bae5b985fafa5e1890
PavelDubinin <pavel.ios.develop@gmail.com>
2024-11-03 21:59:12 +0300

    Initial Commit

---
+++ PhotosMovies/PhotosMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosMoviesApp.swift
+//  PhotosMovies
+//
+//  Created by Павел Дубинин on 03.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotosMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
